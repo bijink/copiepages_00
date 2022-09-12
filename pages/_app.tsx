af09870f5722953d6e9bd43dce5f46105e4a5346
bijink <kbijin528.pro@gmail.com>
2022-09-12 11:15:23 +0530

    Add homePage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-   return <Component {...pageProps} />
+   return (
+      <>
+         <Head>
+            <title>Copiepages_00</title>
+            <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+         </Head>
+
+         <Component {...pageProps} />
+      </>
+   )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next"
 
 const Home: NextPage = () => {
    return (
-      <div>
-         <h1 className="text-5xl">Hello world</h1>
+      <div className="flex h-screen w-full items-center bg-black/20">
+         <div className="container mx-auto flex justify-center  p-6 ">
+            <h1 className="text-5xl">Copiepages_00</h1>
+         </div>
       </div>
    )
 }
